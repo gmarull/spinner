6c038eab4dbb9a1bfbaa361b43029bee0bb59c29
Gerard Marull-Paretas <gerard@teslabs.com>
2021-06-08 15:17:51 +0200

    drivers: svpwm: stm32: fix inclusion order problems
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- drivers/svpwm/svpwm_stm32.c
+++ drivers/svpwm/svpwm_stm32.c
@@ -8,9 +8,8 @@
 #include <soc.h>
 #include <drivers/gpio.h>
 #include <drivers/clock_control/stm32_clock_control.h>
-#include <pinmux/stm32/pinmux_stm32.h>
-
 #include <stm32_ll_tim.h>
+#include <pinmux/stm32/pinmux_stm32.h>
 
 #include <spinner/drivers/currsmp.h>
 #include <spinner/drivers/svpwm.h>
